2c17da046fb4a7935e83cbca3641ac25ff9c80b1
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-27 11:35:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,14 @@ class Student
     end
   
   def self.drop_table 
-    
+    sql =  <<-SQL 
+      DELETE students (
+        id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade TEXT
+          )
+        SQL
+    DB[:conn].execute(sql) 
   end 
   
   
