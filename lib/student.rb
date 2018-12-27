03d633855b05c8fc817e71f7f75b35e63680bfd8
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-27 11:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,37 @@
 class Student
+  attr_accessor :name, :grade 
+  attr_reader :id 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end 
+  
+  def self.create_table
+      sql =  <<-SQL 
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade TEXT
+          )
+          SQL
+      DB[:conn].execute(sql) 
+    end
+  
+  def self.drop_table 
+    
+  end 
+  
+  
+  def save
+    
+  end 
+  
+  
+  def self.create 
+  end 
   
 end
